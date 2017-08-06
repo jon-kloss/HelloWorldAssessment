b02b8e6da621d157c86d59646f866e1b4f43ac26
Jon Kloss <jon.kloss89@gmail.com>
2017-08-06 11:57:08 -0400

    values from forms are bound with ngModel

--- src/src/app/components/register/register.component.ts
+++ src/src/app/components/register/register.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+	firstName: String;
+	lastName: String;
+	address1: String;
+	address2: String;
+	city: String;
+	state: String;
+	zip: Number;
+	country: String;
+	
   constructor() { }
 
   ngOnInit() {
   }
-
+  register()
+  {
+  	const user = {
+  		firstName = this.firstName,
+		lastName = this.lastName,
+		address1 = this.address1,
+		addresss = this.address2,
+		city this.city,
+		state this.state,
+		zip = this.zip,
+		country this.country
+  	}
+  }
 }
